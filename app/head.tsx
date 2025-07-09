836a75d731c704150fe6d1ef3652591d25f8aac6
krishramoju2 <futureai.incorporated@gmail.com>
2025-07-09 05:37:27 +0530

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>UpSkillFam - Empowering Future SDEs</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
+      
     </>
   );
 }
